193203ed11ef8321ea17a403af11ffc148711b5b
post-developer <gambarov.20015960@gmail.com>
2020-01-10 20:25:39 +0500

    ISheet manada system

--- manada/systems/isheet.lua
+++ manada/systems/isheet.lua
@@ -2,14 +2,17 @@ local class = require( "manada.libs.middleclass" )
 
 local ISheet = class( "ISheet" )
 
-function ISheet:initialize( params )
-    
-    self._isheets = {}
+ISheet.SheetsInfoPath = "res.sheets."
+ISheet.SheetsImagePath = "res/sheets/"
 
+function ISheet:initialize( params )
+    self._isheets = {}
 end
 
 function ISheet:add( name )
-    
+    local sheetInfo = require(self.SheetsInfoPath .. name .. "info") 
+    local sheetImage = graphics.newImageSheet(self.SheetsImagePath .. name .. "image.png", sheetInfo:getSheet())
+    self._isheets[name] = { info = sheetInfo, image = sheetImage }
 end
 
 function ISheet:remove( name )
@@ -20,7 +23,6 @@ function ISheet:remove( name )
     end
 
     return false
-
 end
 
 function ISheet:get( name )
@@ -32,13 +34,15 @@ function ISheet:get( name )
     end
 
     return isheet
-
 end
 
 function ISheet:destroy()
 
+    for name, _ in pairs(self._isheets) do
+        self:remove(name)
+    end
+
     self._isheets = nil
-    
 end
 
 return ISheet
