473d7f4e18d4988f2325e509a884f8e558c0d1c0
post-developer <gambarov.20015960@gmail.com>
2020-01-16 22:56:31 +0500

    Square buttons

--- src/scenes/game/ui.lua
+++ src/scenes/game/ui.lua
@@ -1,7 +1,7 @@
 -- need sliding panel
 local widget = require("widget")
 
-local gameObjectFactory = require("src.scenes.game.gameObjects.factories.TestGOFactory"):new()
+local gameObjectFactory = require("src.scenes.game.gameObjects.factories.NPCFactory"):new()
 local barrierFactory = require("src.scenes.game.gameObjects.factories.BarrierFactory"):new()
 
 local uiGroup = display.newGroup()
@@ -10,6 +10,8 @@ local xBtn = 0.1 * display.pixelHeight
 local yBtn = 0.9 * display.pixelWidth
 local widthBtn = 0.15 * display.pixelHeight
 local heightBtn = 0.1 * display.pixelWidth
+local sizeBtn = (widthBtn + heightBtn) / 2
+print(sizeBtn)
 local offsetBtn = 15
 
 local function addButton(displayGroup, text, x, y, handler)
@@ -21,8 +23,8 @@ local function addButton(displayGroup, text, x, y, handler)
             emboss = false,
             -- Properties for a rounded rectangle button
             shape = "roundedRect",
-            width = widthBtn,
-            height = heightBtn,
+            width = sizeBtn,
+            height = sizeBtn,
             cornerRadius = 2,
             fillColor = { default={1,0,0,1}, over={1,0.1,0.7,0.4} },
             strokeColor = { default={1,0.4,0,1}, over={0.8,0.8,1,1} },
@@ -36,19 +38,19 @@ end
 
 addButton(uiGroup, "Spawn characters", xBtn, yBtn, function (event)
     if event.phase == "ended" then
-        manada:getActiveMap():setTouchHandler("characterSpawnHandler", { factory = gameObjectFactory })
+        manada:getActiveMap():setTouchHandler("NPCSpawnHandler", { factory = gameObjectFactory })
     end
 end)
 
 addButton(uiGroup, "Spawn block", xBtn+ widthBtn + offsetBtn, yBtn, function (event)
     if event.phase == "ended" then
-        manada:getActiveMap():setTouchHandler("blockSpawnHandler", { factory = barrierFactory })
+        manada:getActiveMap():setTouchHandler("BlockSpawnHandler", { factory = barrierFactory })
     end
 end)
 
 addButton(uiGroup, "Remove block", xBtn + (widthBtn * 2) + (offsetBtn * 2), yBtn, function (event)
     if event.phase == "ended" then
-        manada:getActiveMap():setTouchHandler("blockRemoveHandler")
+        manada:getActiveMap():setTouchHandler("BlockRemoveHandler")
     end
 end)
 
